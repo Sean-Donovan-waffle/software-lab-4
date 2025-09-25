1ea158892d7515eafc7c07bfbd3ef52bea7b3176
Sean-Donovan-waffle <seandonovan1125@gmail.com>
2025-09-25 14:56:26 -0500

    took out advance funcs

--- calculator.c
+++ calculator.c
@@ -63,22 +63,3 @@ int main() {
 
     return 0;
 }
-
-// Function Definitions
-
-
-double power(double base, double exp) {
-    return pow(base, exp);
-}
-
-double factorial(int n) {
-    double result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
-
-double sqrtFunc(double n) {
-    return sqrt(n);
-}
